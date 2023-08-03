7a0d9505c7b626e24152eda2a989be1287bef897
Eric German <german.eric@gmail.com>
2023-08-03 20:11:06 +0200

    method lance

--- lemonlabs/src/de/main.go
+++ lemonlabs/src/de/main.go
@@ -1,16 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type De struct{ Nbface int }
 
 func (d De) get_face() int {
 	return d.Nbface
 }
+func (d De) lance() int {
+	n := rand.Intn(5) + 1
+	return n
+}
 
 func main() {
 
 	d1 := De{6}
 	fmt.Printf("%v , %T\n", d1, d1)
-	fmt.Printf("nb de face :%v", d1.get_face())
+	fmt.Printf("nb de face :%v\n", d1.get_face())
+	n1 := d1.lance()
+	fmt.Printf("face :%v", n1)
 }
