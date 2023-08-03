dc3b25848f2769634de4a01bae3313752f6e0b79
Eric German <german.eric@gmail.com>
2023-08-03 19:10:00 +0200

    methode golang

--- lemonlabs/src/de/main.go
+++ lemonlabs/src/de/main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-type De = struct{ Nbface int }
+type De struct{ Nbface int }
+
+func (d De) get_face() int {
+	return d.Nbface
+}
 
 func main() {
 
 	d1 := De{6}
-	fmt.Printf("%v , %T", d1, d1)
+	fmt.Printf("%v , %T\n", d1, d1)
+	fmt.Printf("nb de face :%v", d1.get_face())
 }
