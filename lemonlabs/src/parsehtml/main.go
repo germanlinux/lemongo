395732a8c0099e338f34e592b4e928dce0b75ac1
Eric German <german.eric@gmail.com>
2023-08-30 22:26:38 +0200

    search tag

--- lemonlabs/src/parsehtml/main.go
+++ lemonlabs/src/parsehtml/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func readfilehtml(filename string) (string, error) {
@@ -10,13 +14,37 @@ func readfilehtml(filename string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	return string(bst), err
 }
 
+func parse(text string) (data []string) {
+	tkn := html.NewTokenizer(strings.NewReader(text))
+	var vals []string
+	var isH3 bool
+	for {
+		tt := tkn.Next()
+		switch {
+		case tt == html.ErrorToken:
+			return vals
+
+		case tt == html.StartTagToken:
+			t := tkn.Token()
+			isH3 = t.Data == "td"
+		case tt == html.TextToken:
+			t := tkn.Token()
+			if isH3 {
+				vals = append(vals, t.Data)
+			}
+			isH3 = false
+		}
+	}
+}
 func main() {
 	filename := "page.html"
 	text, err := readfilehtml(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	data := parse(text)
+	fmt.Println(data)
 }
