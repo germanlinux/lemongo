4cd35e5c4af2c9938841e67d6c797a16b67de603
Eric German <german.eric@gmail.com>
2023-08-31 19:21:00 +0200

    regexp

--- lemonlabs/src/parsehtml/main.go
+++ lemonlabs/src/parsehtml/main.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"strings"
-
-	"golang.org/x/net/html"
+	"regexp"
 )
 
 func readfilehtml(filename string) (string, error) {
@@ -17,34 +15,50 @@ func readfilehtml(filename string) (string, error) {
 	return string(bst), err
 }
 
-func parse(text string) (data []string) {
-	tkn := html.NewTokenizer(strings.NewReader(text))
-	var vals []string
-	var isH3 bool
-	for {
-		tt := tkn.Next()
-		switch {
-		case tt == html.ErrorToken:
-			return vals
+/*
+	func parse(text string) (data []string) {
+		tkn := html.NewTokenizer(strings.NewReader(text))
+		var vals []string
+		var isH3 bool
+		for {
+			tt := tkn.Next()
 
-		case tt == html.StartTagToken:
-			t := tkn.Token()
-			isH3 = t.Data == "td"
-		case tt == html.TextToken:
-			t := tkn.Token()
-			if isH3 {
-				vals = append(vals, t.Data)
+			switch {
+			case tt == html.ErrorToken:
+				return vals
+
+			case tt == html.StartTagToken:
+				t := tkn.Token()
+				fmt.Printf("token: %v", t)
+				isH3 = t.Data == "td"
+			case tt == html.TextToken:
+				t := tkn.Token()
+				if isH3 {
+					vals = append(vals, t.Data)
+				}
+				isH3 = false
 			}
-			isH3 = false
 		}
 	}
-}
+*/
 func main() {
+	dico := make(map[string]int)
 	filename := "page.html"
 	text, err := readfilehtml(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	data := parse(text)
-	fmt.Println(data)
+	//data := parse(text)
+	//fmt.Println(data)
+	pattern := regexp.MustCompile("[\"2]{1}&gt;</span>([^<]+)")
+	allst := pattern.FindAllStringSubmatch(text, -1)
+	for _, match := range allst {
+		if len(match[1]) > 1 {
+			//	fmt.Printf("%v\n", match[1])
+			dico[match[1]] = 1
+		}
+	}
+	for cle, _ := range dico {
+		fmt.Println(cle)
+	}
 }
