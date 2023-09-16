705dae45454d4ccb3428a48b8cc57979ef85aa43
Eric German <german.eric@gmail.com>
2023-09-16 10:05:25 +0200

    channel

--- lemonlabs/src/channels/main.go
+++ lemonlabs/src/channels/main.go
@@ -1,9 +1,27 @@
 package main
 
-type monchanel <-chan int
-func main {
-c := make(monchanel)
-d := make(monchanel)
-go client(1, c)
-go client(2, d)
+import (
+	"fmt"
+)
+
+type monchanel chan int
+
+func pong(id int, channel chan int) {
+	for {
+		j := <-channel
+		fmt.Printf("id: %v  recu: %v\n", id, j)
+		//time.Sleep(time.Duration(id) * time.Second)
+	}
+}
+func main() {
+	c := make(monchanel)
+
+	go pong(1, c)
+	go pong(2, c)
+	go pong(3, c)
+	go pong(4, c)
+	for i := 0; i < 100; i++ {
+		c <- i
+		//time.Sleep(1 * time.Second)
+	}
 }
