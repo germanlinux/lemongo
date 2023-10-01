e4015bfb8623651f45b5d1a9ebc30b1e0a8d03c9
Eric German <german.eric@gmail.com>
2023-10-01 21:20:47 +0200

    tranches

--- lemonlabs/src/tranches/main.go
+++ lemonlabs/src/tranches/main.go
@@ -21,12 +21,24 @@ func main() {
 	fmt.Println("tranche_a", tranche_a, "capacité:", cap(tranche_a), "taille:", len(tranche_a))
 	tranche_b := tranche_a[1:3]
 	fmt.Println("tranche_b", tranche_b, "capacité:", cap(tranche_b), "taille:", len(tranche_b))
-	tranche_b[0] = 42
+
 	//tranche_a = append(tranche_a, 23)
 	fmt.Println("tranche_a", tranche_a, "capacité:", cap(tranche_a), "taille:", len(tranche_a))
 	fmt.Println("tranche_b", tranche_b, "capacité:", cap(tranche_b), "taille:", len(tranche_b))
-	fmt.Printf("%p a\n", &tranche_a)
-	fmt.Printf("%p b\n", &tranche_b)
+	//	fmt.Printf("%p a\n", &tranche_a)
+	//	fmt.Printf("%p b\n", &tranche_b)
+	tranche_b = append(tranche_b, 18)
+	fmt.Println("tranche_a", tranche_a, "capacité:", cap(tranche_a), "taille:", len(tranche_a))
+	fmt.Println("tranche_b", tranche_b, "capacité:", cap(tranche_b), "taille:", len(tranche_b))
+	//
+	tranche_b = append(tranche_b, 19)
+	tranche_b = append(tranche_b, 20)
+	tranche_b = append(tranche_b, 21)
+	tranche_b = append(tranche_b, 22)
+	tranche_b = append(tranche_b, 23)
+	tranche_b[0] = 99
+	fmt.Println("tranche_a", tranche_a, "capacité:", cap(tranche_a), "taille:", len(tranche_a))
+	fmt.Println("tranche_b", tranche_b, "capacité:", cap(tranche_b), "taille:", len(tranche_b))
 
 	lettres := []string{"etoile", "des", "neiges"}
 	fmt.Printf("Adresse de la variable lettre: %p\n", &lettres)
@@ -58,4 +70,10 @@ func main() {
 	fmt.Println("sub4", sub4, "capacité:", cap(sub4), "taille:", len(sub4))
 	fmt.Println("sub3", sub3, "capacité:", cap(sub3), "taille:", len(sub3))
 	fmt.Println("lettres", lettres, "capacité:", cap(lettres), "taille:", len(lettres))
+	tranche_c := make([]int, 3, 6)
+	tranche_d := tranche_c[1:3:3]
+	tranche_d[1] = 15
+	tranche_d[0] = 99
+	fmt.Println("tranche_c", tranche_c, "capacité:", cap(tranche_c), "taille:", len(tranche_c))
+	fmt.Println("tranche_d", tranche_d, "capacité:", cap(tranche_d), "taille:", len(tranche_d))
 }
