82c09c2d8605e12df99c494573ce9b192d94703c
ericgerman <eric.german@numspot.com>
2023-11-27 19:15:58 +0100

    v0 synchro

--- lemonlabs/src/synchro/main.go
+++ lemonlabs/src/synchro/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/sha256"
-	"database/sql"
 	"encoding/hex"
 	"flag"
 	"fmt"
@@ -11,32 +10,25 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-
-	_ "github.com/mattn/go-sqlite3"
+	"time"
 )
 
-const create string = `
-  CREATE TABLE IF NOT EXISTS fingerprint (
-  path TEXT ,
-  name  TEXT,
-  hash TEXT, 
-  date TEXT,
-  backend TEXT ,
-  PRIMARY KEY (path, name)
-   );`
+
 const backend string = "pro"
 
+type records struct {
+	path string
+	name string 
+	hash string
+	date string
+	backend string
+}
 func storage() (state string) {
 	const file string = "./synchro.db"
-	db, err := sql.Open("sqlite3", file)
-	if err != nil {
-		fmt.Printf("err %v\n", err)
-		panic("stop")
-	}
-	fmt.Println(create)
-	if _, err := db.Exec(create); err != nil {
-		return "error create"
-	}
+	//fmt.Println(create)
+	//if _, err := db.Exec(create); err != nil {
+	//	return "error create"
+	//}
 	return "Ok"
 }
 
@@ -62,17 +54,14 @@ func main() {
 	var pathFlag = flag.String("path", "./", "Chemin complet du repertoire")
 	var repoFlag = flag.String("repo", "pro", "Etiquette de l'instance")
 	flag.Parse()
-	//flag.Usage()
+	flag.Usage()
 	directory := *pathFlag
 	repo := *repoFlag
 	proof := *nFlag
-	fmt.Println(repo, proof)
-	if len(os.Args) > 1 {
-		directory = os.Args[1]
-		// do something with command
-	}
+	madate := time.Now()
+	smadate := madate.Format("02-01-2006")
 	base := util.LastPath(directory)
-	fmt.Println("base:", base)
+	var trecords []records
 	err := filepath.Walk(directory,
 		func(path string, info os.FileInfo, err error) error {
 			var ressource io.Reader
@@ -80,21 +69,22 @@ func main() {
 			if err != nil {
 				return err
 			}
-			fmt.Println("ch:", path)
+//			fmt.Println("ch:", path)
 			if !info.IsDir() {
 				ressource, errfile = os.Open(path)
 				if errfile != nil {
 					log.Fatalln(errfile)
 				}
 				sp, sf := util.ParsePath(path, base)
-				fmt.Println(sp, sf)
-				fmt.Printf("%v => %v\n", path, get_hash(ressource))
-			}
+				//fmt.Println(sp, sf)
+				//fmt.Printf("%v => %v\n", path, get_hash(ressource))
+				trecords = append(trecords,records { sp,sf, get_hash(ressource),smadate, repo} )
+				
 			return nil
-		})
+		}
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println(storage())
-
 }
+	fmt.Println(trecords)
+}	
--- lemonlabs/src/synchro/utils/util.go
+++ lemonlabs/src/synchro/utils/util.go
@@ -11,21 +11,26 @@ func LastPath(chaine string) string {
 	//dir, file := filepath.Split("chaine")
 	parts := strings.Split(chaine, string(os.PathSeparator))
 	if len(parts) == 1 {
+		//fmt.Println("EEE3", chaine)
 		return chaine
 	}
-	fmt.Println("parts last", parts)
+	fmt.Println("EEE4", parts)
+	if parts[len(parts)-1]  == "" {
+		//fmt.Println(("je passe "))
+		return "." + string(os.PathSeparator)
+	} 
 	return parts[len(parts)-1]
 }
 func ParsePath(chaine, base string) (string, string) {
 	dir, file := filepath.Split(chaine)
-	fmt.Printf("on mac  %v, %v\n", dir, file)
+	//fmt.Printf("on mac  %v, %v\n", dir, file)
 	if dir == "" {
 		dir = "." + string(os.PathSeparator)
 		return dir, file
 	}
-	fmt.Printf("on mac  %v, %v\n", dir, file)
+	//fmt.Printf("on mac2 %v, %v, %v\n", dir, file, base)
 	parts := strings.Split(dir, base)
-	fmt.Println("part:", parts)
+	//fmt.Println("part:", parts)
 	pathf := ""
 	if len(parts) > 1 {
 		if parts[1] == "/" {
