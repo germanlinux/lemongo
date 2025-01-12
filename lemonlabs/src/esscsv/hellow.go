6906e959a2325b90d1a5fc191145d1bb7850fcf4
germanlinux <german.eric@gmail.com>
2025-01-12 20:17:30 +0100

    malformed csv

--- lemonlabs/src/esscsv/hellow.go
+++ lemonlabs/src/esscsv/hellow.go
@@ -1,38 +1,58 @@
 package main
 
 import (
-	"encoding/csv"
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 )
 
-var Inp = `"Sample Entry #2","Michael321","12345","https://keepass.info/help/kb/testform.html",""
+/* var Inp = `"Sample Entry #2","Michael321","12345","https://keepass.info/help/kb/testform.html",""
 "pgadmin","german","german","",""
 "microsoft","germanliXXX.fr","XXXXXXXXX","",""
 "zvm","root","rootvmeg","XXXXXXXXX",""
 "collabra","XXXXXX.fr","XXXXXX","https://collabora.dgfip.finances.rie.gouv.fr/",""
 `
+*/
+//"Account","Login Name","Password","Web Site","Comments"
 
 type Entry struct {
-	Name     string `csv:"name"`
-	User     string `csv:"user"`
-	password string `csv:"password"`
+	Name     string `csv:"Account"`
+	User     string `csv:"Login Name"`
+	Password string `csv:"Password"`
+	Url      string `csv:"Web site"`
+	Comment  string `csv:"Comments"`
 }
 
 func main() {
-	r := strings.NewReader(Inp)
-
-	// read csv values using csv.Reader
-	csvReader := csv.NewReader(r)
-	data, err := csvReader.ReadAll()
+	//r := strings.NewReader(Inp)
+	f, err := os.Open("filecsv.csv")
 	if err != nil {
 		log.Fatal(err)
-	}
-	fmt.Println(data)
-	fmt.Printf("data: %T\n", data)
-	for _, item := range data {
-		fmt.Printf("%v %T\n", item, item)
 
+	}
+	defer f.Close()
+
+	// read csv values using csv.Reader
+	//csvReader := csv.NewReader(f)
+	//data, err := csvReader.ReadAll()
+	filescanner := bufio.NewScanner(f)
+	filescanner.Split(bufio.ScanLines)
+	var filelines []string
+	for filescanner.Scan() {
+		filelines = append(filelines, filescanner.Text())
+	}
+	entree := make([]string, 0, 0)
+	for _, item := range filelines {
+		//fmt.Printf("%v\n", item)
+		tligne := strings.Split(item, ",")
+		//fmt.Printf("%v longueur: %v\n", item, len(tligne))
+		if len(tligne) > 4 {
+			entree = append(entree, item)
+		}
+	}
+	for _, lig := range entree {
+		fmt.Printf("%v\n", lig)
 	}
 }
